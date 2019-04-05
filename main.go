1a2b652ff4126c94a2d0b1cd69710236dd04184f
Ivan Rusli <ivan.ru@tnis.com>
2019-04-05 17:25:51 +0700

    make chunking better, more evenly distributed

--- main.go
+++ main.go
@@ -28,17 +28,18 @@ const (
 )
 
 var (
-	timeElapsed            time.Duration
-	timeElapsedSeconds     float64
-	timeElapsedNanoSeconds int64
-	maxJob                 = 10
-	accountListGrouped     = make(map[string][]account)
-	dataToStore            = []accountGroup{}
-	chunkedDataTemp        = [][]account{}
-	chunkedDataWrapper     = [][][]account{}
-	chunkedDataTempLength  int
-	loopCount              int
-	lastLoop               bool
+	timeElapsed                                time.Duration
+	timeElapsedSeconds                         float64
+	timeElapsedNanoSeconds                     int64
+	maxJob                                     = 10
+	accountListGrouped                         = make(map[string][]account)
+	dataToStore                                = []accountGroup{}
+	chunkedDataTempPerDebitSourceAccount       = [][]account{}
+	chunkedDataTempPerDebitSourceAccountLength = 0
+	chunkedDataTemp                            = [][][]account{}
+	chunkedDataWrapper                         = [][][]account{}
+	loopCount                                  int
+	lastLoop                                   bool
 )
 
 func main() {
@@ -60,18 +61,57 @@ func main() {
 		}
 		dataToStore = append(dataToStore, newAccountGroup) //data to store to db later as history
 
-		if len(val)+chunkedDataTempLength > maxJob || lastLoop {
+		fmt.Println("(len(val) + chunkedDataTempPerDebitSourceAccountLength)")
+		fmt.Println((len(val) + chunkedDataTempPerDebitSourceAccountLength))
+		if ((len(val) + chunkedDataTempPerDebitSourceAccountLength) > maxJob) || lastLoop {
 			if lastLoop {
-				chunkedDataTemp = append(chunkedDataTemp, val)
+				chunkedDataTempPerDebitSourceAccount = append(chunkedDataTempPerDebitSourceAccount, val)
 			}
-			chunkedDataWrapper = append(chunkedDataWrapper, chunkedDataTemp)
-			chunkedDataTemp = nil
-			chunkedDataTempLength = 0
+			if chunkedDataTempPerDebitSourceAccountLength == maxJob {
+				chunkedDataWrapper = append(chunkedDataWrapper, chunkedDataTempPerDebitSourceAccount)
+				fmt.Println("chunkedDataWrapperrrrrrrrrrrrrrr")
+				fmt.Println(chunkedDataWrapper)
+			} else {
+				fmt.Println("asdasdasd")
+				if len(chunkedDataTemp) == 0 || (len(val)+chunkedDataTempPerDebitSourceAccountLength) != maxJob {
+					chunkedDataTemp = append(chunkedDataTemp, chunkedDataTempPerDebitSourceAccount)
+				} else {
+					for key, val := range chunkedDataTemp {
+						if len(val) == maxJob {
+							chunkedDataWrapper = append(chunkedDataWrapper, val)
+							continue
+						}
+						if (len(val) + chunkedDataTempPerDebitSourceAccountLength) == maxJob {
+							chunkedDataTemp[key] = append(chunkedDataTemp[key], chunkedDataTempPerDebitSourceAccount...)
+							fmt.Println("chunkedDataTemp[key]")
+							fmt.Println(chunkedDataTemp[key])
+							var accountGroupPerDebitSourceTemp [][]account
+							accountGroupPerDebitSourceTemp = chunkedDataTemp[key]
+							chunkedDataWrapper = append(chunkedDataWrapper, accountGroupPerDebitSourceTemp)
+							// chunkedDataWrapper = append(chunkedDataWrapper, chunkedDataTemp[key])
+							chunkedDataTemp[key] = nil
+							break
+						}
+					}
+				}
+			}
+			chunkedDataTempPerDebitSourceAccount = nil
+			chunkedDataTempPerDebitSourceAccountLength = 0
+		}
+		if lastLoop {
+			fmt.Printf("length : %d\n", len(val))
+			fmt.Println("chunkedDataTemp")
+			fmt.Println(chunkedDataTemp)
+			chunkedDataWrapper = append(chunkedDataWrapper, chunkedDataTemp...)
+			break
 		}
 
-		chunkedDataTemp = append(chunkedDataTemp, val) //chunked data to process auto credit
-		chunkedDataTempLength += len(val)
+		chunkedDataTempPerDebitSourceAccount = append(chunkedDataTempPerDebitSourceAccount, val) //chunked data to process auto credit
+		chunkedDataTempPerDebitSourceAccountLength += len(val)
 		loopCount++
+		fmt.Printf("length : %d\n", len(val))
+		fmt.Println("chunkedDataTempPerDebitSourceAccount")
+		fmt.Println(chunkedDataTempPerDebitSourceAccount)
 	}
 	json.Marshal(dataToStore)
 	// dataToStoreByte, _ := json.Marshal(dataToStore)
